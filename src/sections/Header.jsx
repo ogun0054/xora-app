3dd36b917dfa2b04d37277838800f880a9e2506e
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-09-29 23:12:21 -0400

    development setup done

---
+++ src/sections/Header.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <a href="">
+          <img src="/images/xora.svg" alt="xora-logo" width={115} height={55} />
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
