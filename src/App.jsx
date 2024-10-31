79e8c9c5d211a318ef4c29fc879d0f73d8cfb4cc
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-10-30 21:19:52 -0400

    gh setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
 import Testimonials from "./sections/Testimonials";
+import Downloads from "./sections/Downloads";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Downloads />
+      <Footer />
     </main>
   );
 }
